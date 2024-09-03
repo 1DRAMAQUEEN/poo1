2b74f7aa5f01836a959a9f7940d443a96d5d4aa4
Romina <rominapereira@soymas.cl>
2024-09-03 15:06:31 -0400

    INTRODUCCION A POO

---
+++ point.py
@@ -0,0 +1,20 @@
+#definir objeto punto
+#objeto un conjunto de propiedades y metodos
+#como creamos objetos, se debe crear el conjunto de objetos.
+#"la clase es la categoría" segun la funcionalidad y propiedades que queramos darle. 
+#1°categoría//2°objeto.
+#cateroria = class//categoria 1 tipo.
+#pass para indicar hasta donde llega "tipo break"(donde termina la clase)
+class Point:
+    pass
+p1 = Point()
+p2 = Point()
+
+print(type (p1))
+print(type (10))
+print(type (10.0))
+print(type ("hola"))
+print(type ("adios"))
+print(type(print))
+print(type(input))
+
